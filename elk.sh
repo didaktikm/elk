6f6f463e06d17b9313d6b976cd88a40388963ef9
Pavel <slimjim@slimjim.ru>
2019-07-18 16:07:22 +0300

    add filebeat.conf

--- elk.sh
+++ elk.sh
@@ -3,4 +3,6 @@
 cp /vagrant/id_rsa /home/vagrant/.ssh/
 chown vagrant:vagrant /home/vagrant/.ssh/id_rsa 
 chmod 0600 /home/vagrant/.ssh/id_rsa
+#sed -i -e "\\#PasswordAuthentication yes# s#PasswordAuthentication yes#PasswordAuthentication no#g" /etc/ssh/sshd_config
+sudo service ssh restart
 yum install epel-release vim -y
